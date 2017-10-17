5b0609da2171d285e8b461784f8971a934885f2f
satuevsky <satuevsky@gmail.com>
2017-10-18 01:41:27 +0400

    modified method 'counters.get'

--- src/WebUI/api/counters-get.js
+++ src/WebUI/api/counters-get.js
@@ -1,9 +1,24 @@
 let Method = require('vkl-api').Method;
 
-module.exports = new Method('counters.get', {params: {name: String}}, (req, res) => {
-	res.ok(fakeCounters);
-});
+module.exports = new Method('counters.get', {params: {
+		//seconds to group counters.
+		interval: {type: Number, default: 3600, minValue: 1},
+		//get counters to this date (timestamp/1000).
+		toDate: Number
+	}},
+	async (req, res) => {
+		try{
+			let counters = await req.context.statistics.getCounters({
+				groupByInterval: req.params.interval * 1000,
+				toDate: req.params.toDate * 1000,
+			});
+			res.ok(counters);
+		}catch (e){
+			res.error(-1);
+		}
+	});
 
+/*
 let fakeCounters = [],
 	date = Date.now(),
 	s = 1000,
@@ -31,4 +46,4 @@ while(Date.now() - date < d){
 		}
 	});
 	date -= h;
-}
+}*/
